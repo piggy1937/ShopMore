e98f03501138c9c1de23acd1056060dc971a4d56
piggy123 <Qazxsw123>
2019-11-07 10:10:08 +0800

    修改

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { Result, Button } from 'antd';
 import { withRouter } from 'react-router-dom'
 @withRouter
 class Error extends React.Component{
     render(){
-        return (
-            <Result
-                status="500"
-                title="500"
-                subTitle="对不起，网络出现故障！"
-                extra={<Button type="primary" onClick={()=>{this.props.history.push('/')}}>回到首页</Button>}
-            />
-        )
+        return (<div><Result
+            status="500"
+            title="500"
+            subTitle="Sorry, the server is wrong."
+            extra={<Button type="primary">Back Home</Button>}
+          /></div>)
     }
 }
 export default Error;
